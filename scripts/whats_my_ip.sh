320200853bf895b54c4d20e35c379db49dc9fbe4
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-01-08 09:55:08 +0100

    DOcker host update

--- scripts/whats_my_ip.sh
+++ scripts/whats_my_ip.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1
+export DOCKER_HOST="${DOCKER_HOST:-}"
+
+if [[ "${DOCKER_HOST}" != "" ]]; then
+    echo "${DOCKER_HOST}"
+else
+    ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1
+fi
+
