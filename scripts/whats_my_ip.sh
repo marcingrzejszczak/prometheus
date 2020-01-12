14d30e1d1277e8fd3f126d15d7f4cd968882ef01
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-01-12 21:05:10 +0100

    Fixed prometheus ip setup

--- scripts/whats_my_ip.sh
+++ scripts/whats_my_ip.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export DOCKER_HOST="${DOCKER_HOST:-}"
-
-if [[ "${DOCKER_HOST}" != "" ]]; then
-    echo "${DOCKER_HOST}"
-else
-    ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1
-fi
+# We're interested in the IPs where the apps are running
+ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1
 
